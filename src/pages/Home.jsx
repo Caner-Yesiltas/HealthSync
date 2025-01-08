944d23a8171445b054be265f99438e71dffbfc4f
CanerYesiltas <caneryesiltas1@gmail.com>
2025-01-08 17:00:52 -0500

    feat:add updatelist function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,38 @@
-import React, { useState } from 'react'
-import Doctors from '../components/Doctors'
-import AppointmentList from '../components/AppointmentList'
-import {appointmentData} from '../helper/data'
+import React, { useState } from 'react';
+import Doctors from '../components/Doctors';
+import AppointmentList from '../components/AppointmentList';
+import { appointmentData } from '../helper/data';
 
 const Home = () => {
   const [appointments, setAppointments] = useState(appointmentData);
-  const handleAdd=(newAppointment) =>{
-    setAppointments([...appointments, newAppointment])
+  const handleAdd = (newAppointment) => {
+    setAppointments([...appointments, newAppointment]);
+  };
 
-  }
-
-  const handleDelete=(id)=> {
+  const handleDelete = (id) => {
     console.log(id);
-    const filteredList=appointments.filter(item=>item.id !== id)
-    setAppointments(filteredList)
+    const filteredList = appointments.filter((item) => item.id !== id);
+    setAppointments(filteredList);
+  };
 
+  const handleDoubleClick=(id) => {
+    const updatedList = appointments.map((app)=>
+    app.id === id ? {...app, consulted: !app.consulted} : app);
+    setAppointments(updatedList);
   }
-  return (
-    <main className="text-center mt-2">
-    <h1 className="display-5 text-danger">MASSACHUSETTS GENERAL HOSPITAL</h1>
-    <Doctors handleAdd={handleAdd}  />
-    <AppointmentList appointments={appointments} handleDelete={handleDelete} />
-  </main>
-  )
-}
 
-export default Home
+
+  return (
+    <main className='text-center mt-2'>
+      <h1 className='display-5 text-danger'>MASSACHUSETTS GENERAL HOSPITAL</h1>
+      <Doctors handleAdd={handleAdd} />
+      <AppointmentList
+        appointments={appointments}
+        handleDelete={handleDelete}
+        handleDoubleClick={handleDoubleClick}
+      />
+    </main>
+  );
+};
+
+export default Home;
